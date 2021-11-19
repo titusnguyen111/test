ec716fff57684bc2287d53bc4b8f1e75258c063e
titusnguyen111 <huudat890@gmail.com>
2021-11-19 07:37:53 +0700

    init file

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hello',
+    introDesc: `test is time`,
+    btnIntro: 'nhấp đi',
+    title: 'Lại là test nè',
+    desc: 'Test tiếp ',
+    btnYes: 'Chỗ này thì được nè',
+    btnNo: 'Nhấn vào không được đâu đừng có nhấn tốn lắm',
+    question:'Nhập thử đi gì cũng được',
+    btnReply: 'Gửi đi',
+    reply: 'Bạn nhập chữ gì cũng vậy à',
+    mess: 'test xong r',
+    messDesc: '',
+    btnAccept: 'cám ơn nhiều',
+    messLink: '' //
+}
